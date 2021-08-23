465ba425d8274f235f9a5fde5fb2b872628c6296
Bartek <bartekcebula15@gmail.com>
2021-08-23 19:39:28 +0200

    Working tableview with data from array, some visual improvements

--- Github Users/Users List/Presenter/UsersListPresenter.swift
+++ Github Users/Users List/Presenter/UsersListPresenter.swift
@@ -1,13 +1,19 @@
 import Foundation
 
 class UsersListPresenter {
-    var listOfUsers = [
+    private var searchedUsers: Array<UserModel> = []
+    private var listOfUsers: Array<UserModel> = [
         UserModel(username: "Bartek", avatarURL: "", id: 1),
         UserModel(username: "Adrian", avatarURL: "", id: 2),
-        UserModel(username: "Kuba", avatarURL: "", id: 3)
+        UserModel(username: "Kuba", avatarURL: "", id: 3),
+        UserModel(username: "Bartek2", avatarURL: "", id: 4),
+        UserModel(username: "Adrian2", avatarURL: "", id: 5),
+        UserModel(username: "Kuba2", avatarURL: "", id: 6)
     ]
     
-    func getSearchedUsers(searchedUser: String) {
-        
+    func getSearchedUsers(/*searchedUser: String*/) -> Array<UserModel>{
+        //searchedUsers.removeAll()
+        //return searchedUsers
+        return listOfUsers
     }
 }
--- Github Users/Users List/View/UsersListViewController.swift
+++ Github Users/Users List/View/UsersListViewController.swift
@@ -1,16 +1,38 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var usersListPresenter = UsersListPresenter()
+    private var usersList: Array<UserModel> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        usersList = usersListPresenter.getSearchedUsers()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-    @IBOutlet weak var searchBart: UISearchBar!
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBart: UISearchBar!
     @IBAction func searchButton(_ sender: UIButton) {
-        let _: String = searchBart.text!
-        
+        //let searchedUsername: String = searchBart.text!
     }
     
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+    }
     
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usersList.count
+    }
+
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath);
+
+        cell.textLabel!.text = usersList[indexPath.row].username;
+        
+        return cell;
+    }
 }
